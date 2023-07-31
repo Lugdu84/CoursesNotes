34f19073a09edc4ee92037283e097ac585ce8379
David Grammatico <david.lugdunum@gmail.com>
2023-07-31 11:47:51 +0100

    Initial Commit

---
+++ CoursesNotes/CoursesNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesNotesApp.swift
+//  CoursesNotes
+//
+//  Created by David Grammatico on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoursesNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
